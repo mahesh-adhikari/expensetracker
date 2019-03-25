ed1ec80d5a4a607d8de4f460fbc8be31b2cc5f92
adkmahesh555 <adkmahesh555@gmail.com>
2019-03-26 03:30:46 +0545

    my modificaitons

---
+++ src/views/newdaily.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="card m-4">
+        <div class="card-header">
+            <h3 class="text-info card-title">New Daily Entry</h3>
+        </div>
+        <div class="card-body">
+            <div class="form-group">
+                <label for="title">Title</label>
+                <input type="text" class="form-control" placeholder="Title" v-model="newentry.title">                
+            </div>
+            <div class="form-group">
+                <label for="expense">Expense</label>
+                <input type="number" class="form-control" placeholder="Expense" v-model="newentry.expense">
+            </div>
+            <div class="form-group">
+                <label for="category">Category</label>
+                    <select name="category" id="category" class="form-control" v-model="newentry.category">
+                    <option value="food">Food</option>
+                    <option value="travel">Travel</option>
+                    <option value="finance">Finance</option>
+                </select>
+            </div>
+            <button class="btn btn-success mr-2" @click="createNew">Create</button>
+            <button class="btn btn-danger" @click="cancelCreate">Cancel</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "NewDailyEntry",
+    data(){
+        return {
+            newentry: {
+                title: "",
+                expense: 0,
+                category: ""
+            }
+        }
+    },
+    methods: {
+        createNew(){
+            console.log("Newentry:", JSON.stringify(this.newentry));//create new entry
+            this.$router.replace("/daily")
+        },
+        cancelCreate(){
+            this.$router.replace("/daily");
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/signup.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="card">
+        <div class="card-header">
+            <p class="text-danger text-center lead">Let's create a new account !</p>
+        </div>
+        <div class="card-body">            
+            <input class="form-control" placeholder="Email" v-model="email"> <br>
+            <input class="form-control" placeholder="Password" v-model="password"> <br>
+            <button class="btn btn-primary" @click="signUp">Sign Up</button> <br>
+            <p v-if="showMessage">{{message}}</p>            
+        </div>
+        <div class="card-footer">
+          <router-link to="/login" class="text-warning">Go back to login</router-link>  
+        </div> 
+    </div>
+</template>
+
+<script>
+import firebase from "firebase";
+export default {
+    name: "SignUp",
+    data() {
+        return {
+            email:"",
+            password: "",
+            message: "",
+            showMessage: false
+        }
+    },
+    methods: {
+        signUp: function(){
+            firebase.auth().createUserWithEmailAndPassword(this.email,this.password).then(
+                function(user){
+                    //this.message = "Your account has been created, you can login now from login section!";
+                    //this.showMessage = true;
+                    alert(user);
+                    console.log(user)
+                },
+                function(err){
+                    //this.message = "Oops. " + err.message;
+                    //this.showMessage = true;
+                    alert(err.message);
+                    console.log(err.message)
+                }
+            )
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
