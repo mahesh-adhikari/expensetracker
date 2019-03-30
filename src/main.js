a797a93d30dd90ddbe9537918ad24924605f1dd2
Mahesh <adkmahesh555@gmail.com>
2019-03-30 13:28:08 +0545

    remove what was not supposed to be there

--- src/main.js
+++ src/main.js
@@ -19,12 +19,6 @@ Vue.use(VueSession, false);
 
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyD9EhjfrwCgT7nO0RtpxB3DnC3cul9s4m8",
-  authDomain: "aarambha-abc.firebaseapp.com",
-  databaseURL: "https://aarambha-abc.firebaseio.com",
-  projectId: "aarambha-abc",
-  storageBucket: "aarambha-abc.appspot.com",
-  messagingSenderId: "108719500181"
 };
 firebase.initializeApp(config);
 
